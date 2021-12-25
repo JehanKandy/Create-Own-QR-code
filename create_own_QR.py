ebe7f1857f5520b2c72df61476b2fc9267272686
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-25 07:46:02 +0530

    Update create_own_QR.py

--- create_own_QR.py
+++ create_own_QR.py
@@ -6,5 +6,9 @@ from pyqrcode import QRCode
 
 #create a link for add to the you arcode  
 link="https://www.youtube.com/channel/UC675lo49LTHGi9SCX8XbM5g"
+
+#now create the QRcode
 urls=pyqrcode.create(link)
+
+#QRcode save in image image extentions should be .svg
 urls.svg("jk.svg", scale=8)
