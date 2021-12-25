92d11edc64d0a1d116ed5f16fa033f86916aa3c2
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-25 07:45:16 +0530

    Update create_own_QR.py

--- create_own_QR.py
+++ create_own_QR.py
@@ -1,6 +1,10 @@
+#import pyqrcode module
+
+
 import pyqrcode
 from pyqrcode import QRCode
 
+#create a link for add to the you arcode  
 link="https://www.youtube.com/channel/UC675lo49LTHGi9SCX8XbM5g"
 urls=pyqrcode.create(link)
 urls.svg("jk.svg", scale=8)
